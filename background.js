309c25da27b3ed38f20fa9b21f3697f131edad79
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-05-14 22:17:46 +0200

    DOM Manipulation and bookmarks capture

--- background.js
+++ background.js
@@ -1,12 +1,18 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+
+  const url = changeInfo.url || tab?.url;
+
+  if (url && url.includes('youtube.com/watch')) {
+    const queryParameters = url.split('?')[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    console.log(urlParameters);
   
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+
+    chrome.tabs.sendMessage(tabId, {
+      type: 'NEW',
+      videoId: urlParameters.get('v'),
+    });
+  }
+
+});
